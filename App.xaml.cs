af638bcbb8b5bbb2226b17ef7e9636591e53b12e
rogersm <rogersm@uwosh.edu>
2023-10-09 16:28:48 -0500

    Working project, no comments (by design), to be used as part of Lab 5

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab5_Solution;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AirportsPage();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5_Solution;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
